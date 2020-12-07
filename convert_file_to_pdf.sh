4e3d22c99f663025b1303e23ef61351a1df12126
Richard Nicholas <richardnicholas65@gmail.com>
2020-12-07 09:59:12 +0100

    had the files transposed

--- convert_file_to_pdf.sh
+++ convert_file_to_pdf.sh
@@ -1,1 +1,6 @@
-./convert_file_to_pdf $FILE_TO_CONVERT $OUTPUT_DIRECTORY
+base_name=$(basename -- "${1}")
+extension=${base_name##*.}
+prefix=${base_name%.*}
+
+
+pandoc ${1} -o ${2}/${base_name}.pdf
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,1 @@
-base_name=$(basename -- "$1")
-extension=${base_name##*.}
-prefix=${base_name%.*}
-
-
-pandoc ${1} -o ${2}/${base_name}.pdf
+./convert_file_to_pdf $FILE_TO_CONVERT $OUTPUT_DIRECTORY
