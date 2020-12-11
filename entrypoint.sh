ed9701c8ac5ac8ef1ec1407af40964865c37662d
Richard Nicholas <richardnicholas65@gmail.com>
2020-12-11 13:12:46 +0100

    way to get the directory of the bash source.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ls -l
-./convert_file_to_pdf.sh $FILE_TO_CONVERT $OUTPUT_DIRECTORY
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+$DIR/convert_file_to_pdf.sh $FILE_TO_CONVERT $OUTPUT_DIRECTORY
