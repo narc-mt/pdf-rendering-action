88387ab7a13d9a74282a759652421f76294e76f2
Richard Nicholas <richardnicholas65@gmail.com>
2020-12-15 12:07:41 +0100

    add a random component to the output filename

--- convert_file_to_pdf.sh
+++ convert_file_to_pdf.sh
@@ -3,7 +3,7 @@
 base_name=$(basename -- "${1}")
 extension=${base_name##*.}
 prefix=${base_name%.*}
+outfile="${2}/$RANDOM-${prefix}.pdf"
 
-
-pandoc "${1}" -o "${2}/${prefix}.pdf"
-echo ::set-output name=PDF::"${2}/${prefix}.pdf"
+pandoc "${1}" -o "${outfile}"
+echo ::set-output name=PDF::"$outfile"
